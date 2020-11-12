6a7cfee0c99a0148ec44b0a6b0132292f9c336c8
Luthanh2210 <72242686+Luthanh2210@users.noreply.github.com>
2020-11-12 22:12:42 +0700

    -Display prime numbers less than 100

---
+++ Loop/Bai_tap/bt3/src/primeNumber.java
@@ -0,0 +1,25 @@
+public class primeNumber {
+
+    public static void main(String[] args) {
+        System.out.println("prime number smaller than 100 is: ");
+        for (int i = 0; i < 100; i++) {
+            if (isPrimeNumber(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrimeNumber(int n) {
+        if(n < 2) {
+            return false;
+        }
+
+        int squareRoot = (int)Math.sqrt(n);
+        for (int i = 2; i <=squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+     }
+}
